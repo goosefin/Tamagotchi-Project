31d76fabe6fe4d9a84eeda8d2e60f91994c89b93
goosefin <agustinafin96@gmail.com>
2022-07-06 12:15:17 -0400

    created first morph and age up interval

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
 // alert('You must feed your pet, play with your pet and make sure your pet gets enough sleep. If any of these stats reach 10 your pet will pass on to the after life')
 
 //updates display with the name given by the user
-const tamagotchiName = prompt('What do you want to name your pet')
-const h1 = document.querySelector('h1')
-h1.innerText = tamagotchiName;
+//const tamagotchiName = prompt('What do you want to name your pet')
+//const h1 = document.querySelector('h1')
+//h1.innerText = tamagotchiName;
 
 //creates the Tamagotchi class
 class Tamagotchi {
-    constructor(){
+    constructor(hunger,sleep,play,age){
         this.hunger = 5
         this.sleep = 5
         this.play = 5
@@ -18,8 +18,8 @@ class Tamagotchi {
     feed() {
         if(this.hunger >= 0){
             this.hunger -= 1
-        }  
-    }
+        }
+    }  
     rest(){
         if(this.sleep >= 0){
             this.sleep -= 1
@@ -30,24 +30,31 @@ class Tamagotchi {
             this.play -= 1
         }  
     }
-
-}
-
-//ages the pet every 10 seconds
-let ageStat = document.querySelector('#age')
-let counter = 0
-/*setInterval(function () {
-    counter += 1
-    ageStat.innerText = counter
-}, 10000)*/
-
-//morphs once the pet is 5 
-if(ageStat === 5){
-    alert('Your pet is 5! Watch them morph!!')
 }
 
 //instantiates the class
 const pet = new Tamagotchi
+let ageCounter = 0
+
+// let ageStat = document.querySelector('#age')
+// ageStat.innerText = pet.age
+// console.log(pet.age)
+
+//ages the pet every 10 seconds
+const age = () => {
+    let ageStat = document.querySelector('#age')
+    ageCounter += 1
+    ageStat.innerText = ageCounter
+    if(ageCounter == 5){
+        alert('Your pet is 5! Watch them morph!!')
+        document.querySelector('#egg').setAttribute('src','https://i.imgur.com/YsEIIVQ.png')
+    }
+}
+//setInterval(age, 1000)
+
+
+//morphs once the pet is 5 
+
 
 //checks if pet died
 if(pet.hunger === 10 || pet.sleep === 10 || pet.play === 10){
@@ -74,8 +81,16 @@ const lightToggle = () => {
 
 
 //event listeners
-document.getElementById('feed-button').addEventListener('click', pet.feed)
-document.getElementById('sleep-button').addEventListener('click', pet.rest)
-document.getElementById('play-button').addEventListener('click', pet.playTime)
+//document.getElementById('feed-button').addEventListener('click', feed)
+//document.getElementById('sleep-button').addEventListener('click', pet.rest)
+//document.getElementById('play-button').addEventListener('click', pet.playTime)
 document.getElementById('dark-switch').addEventListener('click',darkToggle)
 document.getElementById('light-switch').addEventListener('click',lightToggle)
+
+//Questions
+//Where can I put my DOM manupulation to update when my methods inside of the Tamagotchi class fire?
+    //When i created a function outside of the constructor it ran fine
+    //Should i move all of my methods outside of the class declaration?
+
+//How do I word my event listener do that it fires on tamagotchi object that i created?
+    //I've tried instantiating a new class and then dding it to the even listener like on line 89 but it doesnt fire 
