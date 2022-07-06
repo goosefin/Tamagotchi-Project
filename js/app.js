7ae34ae463e4f43ff19a9671e8df89ca22c75356
goosefin <agustinafin96@gmail.com>
2022-07-05 20:31:18 -0400

    all buttons created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log('Sanity check')
+// alert('Welcome to my tamagotchi game!')
+// alert('Here are the rules:')
+// alert('You must feed your pet, play with your pet and make sure your pet gets enough sleep. If any of these stats reach 10 your pet will pass on to the after life')
+
